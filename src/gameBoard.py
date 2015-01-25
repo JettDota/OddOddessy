50c6badb63d3e5f4c464602ea450daae333679ab
jet <jetlewis@ucdavis.edu>
2015-01-24 17:06:41 -0800

    fixed that shit

--- src/gameBoard.py
+++ src/gameBoard.py
@@ -5,25 +5,18 @@ Created on Jan 24, 2015
 '''
 import pygame
 from platform import platform
-from pygame.examples.headless_no_windows_needed import screen
 class gameBoard(object):
-    '''
-    classdocs
-    '''
     def __init__(self):
-        '''
-        Constructor
-        '''
         self.size = 256
         self.pixelsize = 256*32
         self.pixelloc = 0
-        self.platforms = [[None for x in range (256)]for x in range (20)]
+        self.platforms = [[-1 for x in range (256)]for y in range (0,20)]
         for i in range(0,256):
-            self.platforms[19][i]=platform(i,19)
+            self.platforms[19][i]=platform(i,18)
     def draw(self,screen):
         for i in range(20):
-            for j in range((int)(self.pixelloc/32)-1,(int)(self.pixelloc/32)+33):
-                if(self.platforms[i][j]!=None):
+            for j in range((int)(self.pixelloc/32),(int)(self.pixelloc/32)+33):
+                if(self.platforms[i][j]!=-1):
                     self.platforms[i][j].draw(screen,self.pixelloc)
             
     def moveScreen(self,right):
@@ -34,7 +27,7 @@ class gameBoard(object):
     def checkCol(self,x,y):
         for i in range(0,20):
             for j in range(0,256):
-                if(self.platforms[i][j]!=None):
+                if(self.platforms[i][j]!=-1):
                     if(i*32<y and i*32+32>y):
                         if(j*32<x and j*32+32>x):
                             return True
